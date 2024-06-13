177a7ab571a26f4f37187f904dc68c4ea86b1e3a
Gab0doulos <38771374+Gab0doulos@users.noreply.github.com>
2024-06-12 23:19:47 -0600

    Update conexion.php

--- Modelos/conexion.php
+++ Modelos/conexion.php
@@ -3,9 +3,9 @@
 class Conexion{
     static public function conectar(){
         try{
-            $conn = new PDO("mysql:host=localhost;dbname=la_cosecha","root","",array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+            //$conn = new PDO("mysql:host=localhost;dbname=la_cosecha","root","",array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
 
-            //$conn = new PDO("mysql:host=rds-instance.c78kai0qwest.us-east-2.rds.amazonaws.com;dbname=la_cosecha","admin","Calcetin1994!",array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+            $conn = new PDO("mysql:host=rds-instance.c78kai0qwest.us-east-2.rds.amazonaws.com;dbname=la_cosecha","admin","Calcetin1994!",array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
             return $conn;
 
         }
