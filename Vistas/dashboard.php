cca8e5bb33dcaa42fe1ce34f646db3b5b3c24f21
Gab0doulos <proskuneo77@gmail.com>
2024-06-21 12:36:57 -0600

    agregando boton para contraer menu lateral

--- Vistas/dashboard.php
+++ Vistas/dashboard.php
@@ -3,12 +3,21 @@
     <div class="container-fluid">
         <div class="row mb-2">
             <div class="col-sm-6">
-                <h1 class="m-0">Tablero Principal</h1>
+                <h1 class="m-0">Tablero Principal hola</h1>
+                <a href="#" class="brand-link toggle-btn">
+                    <i class="fas fa-bars"></i>
+                </a>
             </div><!-- /.col -->
             <div class="col-sm-6">
                 <ol class="breadcrumb float-sm-right">
                     <li class="breadcrumb-item"><a href="#">Inicio</a></li>
                     <li class="breadcrumb-item active">Tablero Principal</li>
+                    <li>
+                        <!-- Botón o enlace para mostrar/ocultar el sidebar -->
+                        <a href="#" class="btn btn-default" id="toggleSidebarBtn">
+                            <i class="fas fa-bars"></i> Mostrar/Esconder Menú
+                        </a>
+                    </li>
                 </ol>
             </div><!-- /.col -->
         </div><!-- /.row -->
@@ -130,7 +139,7 @@
         <div class="row">
             <div class="col-12">
                 <div class="card card-info">
-                    <div class="card-header"> 
+                    <div class="card-header">
                         <h3 class="card-title" id="TituloCodBarras"></h3>
                         <div class="card-tools">
                             <button type="button" class="btn btn-tool" data-card-widget="collapse">
@@ -299,165 +308,174 @@
 
 
                     });
-                },20000);
+                }, 20000);
 
                 /* =======================================================
     SOLICITUD AJAX GRAFICO DE BARRAS DE VENTAS DEL MES
     =======================================================*/
-    $.ajax({
-        url: "ajax/dashboard.ajax.php",
-        method: 'POST',
-        data: {
-            'accion': 1 //parametro para obtener las ventas del mes
-        },
-        dataType: 'json',
-        success: function(respuesta) {
-            // console.log("respuesta", respuesta);
+                $.ajax({
+                    url: "ajax/dashboard.ajax.php",
+                    method: 'POST',
+                    data: {
+                        'accion': 1 //parametro para obtener las ventas del mes
+                    },
+                    dataType: 'json',
+                    success: function(respuesta) {
+                        // console.log("respuesta", respuesta);
 
-            var fecha_venta = [];
-            var total_venta = [];
-            var total_venta_ant = [];
+                        var fecha_venta = [];
+                        var total_venta = [];
+                        var total_venta_ant = [];
 
-            var total_ventas_mes = 0;
+                        var total_ventas_mes = 0;
 
-            for (let i = 0; i < respuesta.length; i++) {
+                        for (let i = 0; i < respuesta.length; i++) {
 
-                fecha_venta.push(respuesta[i]['fecha_venta']);
-                total_venta.push(respuesta[i]['total_venta']);
-                total_venta_ant.push(respuesta[i]['total_venta_ant']);
-                total_ventas_mes = parseFloat(total_ventas_mes) + parseFloat(respuesta[i][
-                    'total_venta'
-                ]);
+                            fecha_venta.push(respuesta[i]['fecha_venta']);
+                            total_venta.push(respuesta[i]['total_venta']);
+                            total_venta_ant.push(respuesta[i]['total_venta_ant']);
+                            total_ventas_mes = parseFloat(total_ventas_mes) + parseFloat(respuesta[
+                                i][
+                                'total_venta'
+                            ]);
 
-            }
+                        }
 
-            total_venta.push(0);
-            // total_venta.push(600);
+                        total_venta.push(0);
+                        // total_venta.push(600);
 
-            // console.log(total_ventas_mes);
+                        // console.log(total_ventas_mes);
 
-            $("#TituloCodBarras").html('Ventas del Mes: Q./ ' + total_ventas_mes.toString().replace(
-                /\d(?=(\d{3})+\.)/g, "$&,"));
+                        $("#TituloCodBarras").html('Ventas del Mes: Q./ ' + total_ventas_mes
+                            .toString().replace(
+                                /\d(?=(\d{3})+\.)/g, "$&,"));
 
-            var barChartCanvas = $("#barChart").get(0).getContext('2d');
+                        var barChartCanvas = $("#barChart").get(0).getContext('2d');
 
-            var areaChartData = {
-                labels: fecha_venta,
-                datasets: [{
-                    label: 'Ventas del Mes - Enero 2022',
-                    backgroundColor: 'rgba(60,141,188,0.9)',
-                    data: total_venta
-                }]
-            }
+                        var areaChartData = {
+                            labels: fecha_venta,
+                            datasets: [{
+                                label: 'Ventas del Mes - Enero 2022',
+                                backgroundColor: 'rgba(60,141,188,0.9)',
+                                data: total_venta
+                            }]
+                        }
 
-            var barChartData = $.extend(true, {}, areaChartData);
+                        var barChartData = $.extend(true, {}, areaChartData);
 
-            var temp0 = areaChartData.datasets[0];
+                        var temp0 = areaChartData.datasets[0];
 
-            barChartData.datasets[0] = temp0;
+                        barChartData.datasets[0] = temp0;
 
-            var barChartOptions = {
-                maintainAspectRatio: false,
-                responsive: true,
-                events: false,
-                legend: {
-                    display: true
-                },
-                scales: {
-                    xAxes: [{
-                        stacked: true,
-                    }],
-                    yAxes: [{
-                        stacked: true
-                    }]
-                },
-                animation: {
-                    duration: 500,
-                    easing: "easeOutQuart",
-                    onComplete: function() {
-                        var ctx = this.chart.ctx;
-                        ctx.font = Chart.helpers.fontString(Chart.defaults.global
-                            .defaultFontFamily, 'normal',
-                            Chart.defaults.global.defaultFontFamily);
-                        ctx.textAlign = 'center';
-                        ctx.textBaseline = 'bottom';
+                        var barChartOptions = {
+                            maintainAspectRatio: false,
+                            responsive: true,
+                            events: false,
+                            legend: {
+                                display: true
+                            },
+                            scales: {
+                                xAxes: [{
+                                    stacked: true,
+                                }],
+                                yAxes: [{
+                                    stacked: true
+                                }]
+                            },
+                            animation: {
+                                duration: 500,
+                                easing: "easeOutQuart",
+                                onComplete: function() {
+                                    var ctx = this.chart.ctx;
+                                    ctx.font = Chart.helpers.fontString(Chart.defaults
+                                        .global
+                                        .defaultFontFamily, 'normal',
+                                        Chart.defaults.global.defaultFontFamily);
+                                    ctx.textAlign = 'center';
+                                    ctx.textBaseline = 'bottom';
 
-                        this.data.datasets.forEach(function(dataset) {
-                            for (var i = 0; i < dataset.data.length; i++) {
-                                var model = dataset._meta[Object.keys(dataset
-                                        ._meta)[0]].data[i]._model,
-                                    scale_max = dataset._meta[Object.keys(dataset
-                                        ._meta)[0]].data[i]._yScale.maxHeight;
-                                ctx.fillStyle = '#444';
-                                var y_pos = model.y - 5;
-                                // Make sure data value does not get overflown and hidden
-                                // when the bar's value is too close to max value of scale
-                                // Note: The y value is reverse, it counts from top down
-                                if ((scale_max - model.y) / scale_max >= 0.93)
-                                    y_pos = model.y + 20;
-                                ctx.fillText(dataset.data[i], model.x, y_pos);
+                                    this.data.datasets.forEach(function(dataset) {
+                                        for (var i = 0; i < dataset.data
+                                            .length; i++) {
+                                            var model = dataset._meta[Object.keys(
+                                                    dataset
+                                                    ._meta)[0]].data[i]._model,
+                                                scale_max = dataset._meta[Object
+                                                    .keys(dataset
+                                                        ._meta)[0]].data[i]._yScale
+                                                .maxHeight;
+                                            ctx.fillStyle = '#444';
+                                            var y_pos = model.y - 5;
+                                            // Make sure data value does not get overflown and hidden
+                                            // when the bar's value is too close to max value of scale
+                                            // Note: The y value is reverse, it counts from top down
+                                            if ((scale_max - model.y) / scale_max >=
+                                                0.93)
+                                                y_pos = model.y + 20;
+                                            ctx.fillText(dataset.data[i], model.x,
+                                                y_pos);
+                                        }
+                                    });
+                                }
                             }
-                        });
+                        }
+
+                        new Chart(barChartCanvas, {
+                            type: 'bar',
+                            data: barChartData,
+                            options: barChartOptions
+                        })
+
+
                     }
-                }
-            }
+                });
 
-            new Chart(barChartCanvas, {
-                type: 'bar',
-                data: barChartData,
-                options: barChartOptions
+
+                $.ajax({
+                    url: "ajax/dashboard.ajax.php",
+                    type: "POST",
+                    data: {
+                        'accion': 2 // listar los 10 productos mas vendidos
+                    },
+                    dataType: 'json',
+                    success: function(respuesta) {
+                        // console.log("respuesta",respuesta);
+
+                        for (let i = 0; i < respuesta.length; i++) {
+                            filas = '<tr>' +
+                                '<td>' + respuesta[i]["codigo_producto"] + '</td>' +
+                                '<td>' + respuesta[i]["descripcion_producto"] + '</td>' +
+                                '<td>' + respuesta[i]["cantidad"] + '</td>' +
+                                '<td> Q./ ' + respuesta[i]["total_venta"] + '</td>' +
+                                '</tr>'
+                            $("#tbl_productos_mas_vendidos tbody").append(filas);
+                        }
+
+                    }
+                });
+
+                $.ajax({
+                    url: "ajax/dashboard.ajax.php",
+                    type: "POST",
+                    data: {
+                        'accion': 3 // listar los  productos con poco stock
+                    },
+                    dataType: 'json',
+                    success: function(respuesta) {
+                        // console.log("respuesta",respuesta);
+
+                        for (let i = 0; i < respuesta.length; i++) {
+                            filas = '<tr>' +
+                                '<td>' + respuesta[i]["codigo_producto"] + '</td>' +
+                                '<td>' + respuesta[i]["descripcion_producto"] + '</td>' +
+                                '<td>' + respuesta[i]["stock_producto"] + '</td>' +
+                                '<td> Q./ ' + respuesta[i]["minimo_stock_producto"] + '</td>' +
+                                '</tr>'
+                            $("#tbl_productos_poco_stock tbody").append(filas);
+                        }
+
+                    }
+                });
+
             })
-
-
-        }
-    });
-
-
-    $.ajax({
-        url: "ajax/dashboard.ajax.php",
-        type: "POST",
-        data: {
-            'accion': 2 // listar los 10 productos mas vendidos
-        },
-        dataType:'json',
-        success:function(respuesta){
-            // console.log("respuesta",respuesta);
-
-            for (let i = 0; i < respuesta.length; i++) {
-                filas = '<tr>'+
-                            '<td>'+ respuesta[i]["codigo_producto"] + '</td>'+
-                            '<td>'+ respuesta[i]["descripcion_producto"] + '</td>'+
-                            '<td>'+ respuesta[i]["cantidad"] + '</td>'+
-                            '<td> Q./ '+ respuesta[i]["total_venta"] + '</td>'+
-                        '</tr>'
-                $("#tbl_productos_mas_vendidos tbody").append(filas);
-            }
-            
-        }
-    });
-
-    $.ajax({
-        url: "ajax/dashboard.ajax.php",
-        type: "POST",
-        data: {
-            'accion': 3 // listar los  productos con poco stock
-        },
-        dataType:'json',
-        success:function(respuesta){
-            // console.log("respuesta",respuesta);
-
-            for (let i = 0; i < respuesta.length; i++) {
-                filas = '<tr>'+
-                            '<td>'+ respuesta[i]["codigo_producto"] + '</td>'+
-                            '<td>'+ respuesta[i]["descripcion_producto"] + '</td>'+
-                            '<td>'+ respuesta[i]["stock_producto"] + '</td>'+
-                            '<td> Q./ ' + respuesta[i]["minimo_stock_producto"] + '</td>'+
-                        '</tr>'
-                $("#tbl_productos_poco_stock tbody").append(filas);
-            }
-            
-        }
-    });
-
-})
- </script>
+            </script>
